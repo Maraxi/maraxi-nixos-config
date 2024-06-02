c7d809d70daa21f856a884e7c901e8641b1c92e3
Maraxi <Maraxi@users.noreply.github.com>
2024-06-02 10:46:30 +0200

    enable dconf

--- hosts/nixos-laptop/configuration.nix
+++ hosts/nixos-laptop/configuration.nix
@@ -117,7 +117,7 @@
     };
   };
 
-  # programs.dconf.enable = true;
+  programs.dconf.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
