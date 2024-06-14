022ed8fe3aafb26a22b79385fcfcb52e56dc1bc0
Maraxi <Maraxi@users.noreply.github.com>
2024-06-14 15:15:32 +0200

    git aliases

--- user/git.nix
+++ user/git.nix
@@ -4,9 +4,13 @@
     userName = "Maraxi";
     userEmail = "Maraxi@users.noreply.github.com";
     aliases = {
+      aliases = "config --get-regexp alias";
+      pushd = "push -u origin HEAD";
       s = "status";
       p = "add -p";
       cm = "commit -m";
+      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --branches --remotes";
+      lgs = "lg -n 15";
     };
     extraConfig = {
       init.defaultBranch = "main";
