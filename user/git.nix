f097e62a66b74659aef9c10c8ec7f2af082d1b25
Maraxi <Maraxi@users.noreply.github.com>
2024-07-23 22:34:21 +0200

    better lg for git

--- user/git.nix
+++ user/git.nix
@@ -9,7 +9,7 @@
       s = "status";
       p = "add -p";
       cm = "commit -m";
-      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --branches --remotes";
+      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --branches --remotes --tags HEAD";
       lgs = "lg -n 15";
     };
     extraConfig = {
