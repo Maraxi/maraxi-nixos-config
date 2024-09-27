d2c7da652253e9686e1116bf36cc21802d7f4b68
stefan.eysoldt <stefan.eysoldt@ecc.de>
2024-09-27 22:49:07 +0200

    remove unused home manager config

--- flake.nix
+++ flake.nix
@@ -29,15 +29,6 @@
         inherit inputs;
       };
     };
-    homeConfigurations."stefan@ubuntu" = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-      modules = [
-        ./hosts/ubuntu-thinkpad/home.nix
-      ];
-    };
     nixosConfigurations."stefan-nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
