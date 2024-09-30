6858f9c41301611851f11e227c9cb306b38038be
Maraxi <Maraxi@users.noreply.github.com>
2024-09-30 20:58:50 +0200

    seperate imports for home config

--- user/default.nix
+++ user/default.nix
@@ -6,18 +6,27 @@
   outputs,
   ...
 }: {
-  imports = [
-    ./alacritty.nix
-    ./environment.nix
-    ./git.nix
-    ./gtk.nix
-    ./packages.nix
-    ./shells.nix
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
-      sha256 = "01dkfr9wq3ib5hlyq9zq662mp0jl42fw3f6gd2qgdf8l8ia78j7i";
-    })
-  ];
+  imports = let
+    specific-imports =
+      if setup.isNixOS
+      then [
+      ]
+      else [
+        (builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
+          sha256 = "01dkfr9wq3ib5hlyq9zq662mp0jl42fw3f6gd2qgdf8l8ia78j7i";
+        })
+      ];
+  in
+    [
+      ./alacritty.nix
+      ./environment.nix
+      ./git.nix
+      ./gtk.nix
+      ./packages.nix
+      ./shells.nix
+    ]
+    ++ specific-imports;
 
   programs.home-manager.enable = true;
 
