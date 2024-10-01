ad10c64bb41a665f9206e4043b4f8ede8f0b39a6
stefan.eysoldt <stefan.eysoldt@ecc.de>
2024-10-01 10:07:59 +0200

    git shorthands

--- user/git.nix
+++ user/git.nix
@@ -1,4 +1,8 @@
 {...}: {
+  home.shellAliases = {
+    s = "git status";
+    g = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' -n5";
+  };
   programs.git = {
     enable = true;
     userName = "Maraxi";
