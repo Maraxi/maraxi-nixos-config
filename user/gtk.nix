67909ad03a6af83282c7a266fcc2e55777d26654
Maraxi <Maraxi@users.noreply.github.com>
2024-10-03 14:55:47 +0200

    add home pointercursor

--- user/gtk.nix
+++ user/gtk.nix
@@ -1,4 +1,10 @@
 {pkgs, ...}: {
+  home.pointerCursor = {
+    name = "volantes_cursors";
+    package = pkgs.volantes-cursors;
+    gtk.enable = true;
+    x11.enable = true;
+  };
   gtk = {
     enable = true;
     theme = {
