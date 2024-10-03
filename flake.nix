ba9733504040337ecb7f6455a6ceea0ad6f19a65
Maraxi <Maraxi@users.noreply.github.com>
2024-10-03 15:56:41 +0200

    Add kickstart flake

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    kickstart-nix-nvim = {
+      url = "github:Maraxi/kickstart-nix.nvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixGL = {
       url = "github:nix-community/nixGL/310f8e49a149e4c9ea52f1adf70cdc768ec53f8a";
       inputs.nixpkgs.follows = "nixpkgs";
