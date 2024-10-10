bb266ba8afe460b030405d9e054c2d65118d9ffa
stefan.eysoldt <stefan.eysoldt@ecc.de>
2024-10-10 15:53:11 +0200

    neovim 0.9.5 for non-nixOS

--- user/neovim.nix
+++ user/neovim.nix
@@ -1,3 +1,32 @@
 {
-  programs.neovim.enable = true;
+  setup,
+  pkgs,
+  ...
+}: {
+  # programs.neovim = {
+  # enable = true;
+  # package = let
+  # nvim_pkgs =
+  # if setup.isNixOS
+  # then pkgs
+  # else
+  # import (builtins.fetchTarball {
+  # name = "pkgs-neovim-0.9.5";
+  # url = "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
+  # sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
+  # }) {inherit (pkgs) system;};
+  # in
+  # nvim_pkgs.neovim;
+  # };
+  home.packages = let
+    nvim_pkgs =
+      if setup.isNixOS
+      then pkgs
+      else
+        import (builtins.fetchTarball {
+          name = "pkgs-neovim-0.9.5";
+          url = "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
+          sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
+        }) {inherit (pkgs) system;};
+  in [nvim_pkgs.neovim];
 }
