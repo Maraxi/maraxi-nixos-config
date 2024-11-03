e360f1c719db6a144c205b2bc7294c91d60fa138
stefan.eysoldt <stefan.eysoldt@ecc.de>
2024-11-03 11:04:57 +0100

    set proxy var

--- user/ecc.nix
+++ user/ecc.nix
@@ -1,6 +1,7 @@
 {
   home.sessionVariables = {
     UV_INDEX_URL = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
+    no_proxy = "$no_proxy,api.ocp.dev.eex.energy";
   };
   home.shellAliases = {
     cal = "ncal -b3A3";
