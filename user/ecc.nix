379f788a3026518a44430bb4976c6adadfdf4f6c
Maraxi <Maraxi@users.noreply.github.com>
2024-11-08 15:32:56 +0100

    move uv python var

--- user/ecc.nix
+++ user/ecc.nix
@@ -1,5 +1,6 @@
 {
   home.sessionVariables = {
+    UV_PYTHON = "3.11";
     UV_INDEX_URL = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
     no_proxy = "$no_proxy,api.ocp.dev.eex.energy";
   };
--- user/environment.nix
+++ user/environment.nix
@@ -6,7 +6,6 @@
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
-    UV_PYTHON = "3.11";
   };
   home.sessionPath = [
     "$HOME/bin"
