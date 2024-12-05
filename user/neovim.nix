3396b00b00c056d915b8b1ebc1970860c5fa3f87
Maraxi <Maraxi@users.noreply.github.com>
2024-12-06 00:00:47 +0100

    Add lua-lsp to packages

--- user/neovim.nix
+++ user/neovim.nix
@@ -1,9 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   programs.neovim = {
     enable = true;
     vimAlias = true;
     # vimdiffAlias = true;
   };
+
+  # dynamic lsps
+  home.packages = with pkgs; [
+    luajitPackages.lua-lsp
+  ];
 
   # xdg.configFile."nvim" = {
   #   recursive = true;
