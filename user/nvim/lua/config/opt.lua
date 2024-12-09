217faa86e23971dec1638f557e5aa6c19d2a3686
Maraxi <Maraxi@users.noreply.github.com>
2024-12-09 22:48:58 +0100

    Add more nvim config

--- user/nvim/lua/config/opt.lua
+++ user/nvim/lua/config/opt.lua
@@ -1,6 +1,5 @@
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -8,6 +7,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.shiftwidth = 4
+
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
 
 vim.opt.clipboard = "unnamedplus"
 
