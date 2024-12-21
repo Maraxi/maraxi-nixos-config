96004583e701481595e244a33e3bd5ffcf132e29
stefan.eysoldt <stefan.eysoldt@ecc.de>
2024-12-21 10:04:52 +0100

    fix uv env variable

--- user/ecc.nix
+++ user/ecc.nix
@@ -1,7 +1,7 @@
 {
   home.sessionVariables = {
     UV_PYTHON = "3.11";
-    UV_INDEX_URL = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
+    UV_DEFAULT_INDEX = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
     UV_PYTHON_PREFERENCE = "only-managed";
     no_proxy = "$no_proxy,api.ocp.dev.eex.energy";
   };
