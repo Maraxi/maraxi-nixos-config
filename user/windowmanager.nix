7d4d969dda46ed09147573b3fe70895fc0f92fa6
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-01-06 13:09:31 +0100

    numbered workspaces

--- user/windowmanager.nix
+++ user/windowmanager.nix
@@ -96,6 +96,28 @@
       "${meh}+f" = "fullscreen toggle";
       "${meh}+l" = "floating toggle"; # toggle tiling / floating for active window
       "${meh}+o" = "focus mode_toggle"; # change focus between tiling / floating windows
+
+      "${modifier}+1" = "workspace number 1";
+      "${modifier}+2" = "workspace number 2";
+      "${modifier}+3" = "workspace number 3";
+      "${modifier}+4" = "workspace number 4";
+      "${modifier}+5" = "workspace number 5";
+      "${modifier}+6" = "workspace number 6";
+      "${modifier}+7" = "workspace number 7";
+      "${modifier}+8" = "workspace number 8";
+      "${modifier}+9" = "workspace number 9";
+      "${modifier}+0" = "workspace number 10";
+
+      "${modifier}+Shift+1" = "move container to workspace number 1";
+      "${modifier}+Shift+2" = "move container to workspace number 2";
+      "${modifier}+Shift+3" = "move container to workspace number 3";
+      "${modifier}+Shift+4" = "move container to workspace number 4";
+      "${modifier}+Shift+5" = "move container to workspace number 5";
+      "${modifier}+Shift+6" = "move container to workspace number 6";
+      "${modifier}+Shift+7" = "move container to workspace number 7";
+      "${modifier}+Shift+8" = "move container to workspace number 8";
+      "${modifier}+Shift+9" = "move container to workspace number 9";
+      "${modifier}+Shift+0" = "move container to workspace number 10";
     };
   };
 in
@@ -161,28 +183,6 @@ in
           in
             shared_config.keybindings
             // {
-              "${modifier}+1" = "workspace number 1";
-              "${modifier}+2" = "workspace number 2";
-              "${modifier}+3" = "workspace number 3";
-              "${modifier}+4" = "workspace number 4";
-              "${modifier}+5" = "workspace number 5";
-              "${modifier}+6" = "workspace number 6";
-              "${modifier}+7" = "workspace number 7";
-              "${modifier}+8" = "workspace number 8";
-              "${modifier}+9" = "workspace number 9";
-              "${modifier}+0" = "workspace number 10";
-
-              "${modifier}+Shift+1" = "move container to workspace number 1";
-              "${modifier}+Shift+2" = "move container to workspace number 2";
-              "${modifier}+Shift+3" = "move container to workspace number 3";
-              "${modifier}+Shift+4" = "move container to workspace number 4";
-              "${modifier}+Shift+5" = "move container to workspace number 5";
-              "${modifier}+Shift+6" = "move container to workspace number 6";
-              "${modifier}+Shift+7" = "move container to workspace number 7";
-              "${modifier}+Shift+8" = "move container to workspace number 8";
-              "${modifier}+Shift+9" = "move container to workspace number 9";
-              "${modifier}+Shift+0" = "move container to workspace number 10";
-
               "${modifier}+Shift+comma" = "move scratchpad";
               "${modifier}+comma" = "scratchpad show";
 
@@ -348,43 +348,6 @@ in
         # scratchpad
         bindsym $mod+Shift+comma move scratchpad
         bindsym $mod+comma scratchpad show
-
-        # Define names for default workspaces for which we configure key bindings later on.
-        # We use variables to avoid repeating the names in multiple places.
-        set $ws1 "1"
-        set $ws2 "2"
-        set $ws3 "3"
-        set $ws4 "4"
-        set $ws5 "5"
-        set $ws6 "6"
-        set $ws7 "7"
-        set $ws8 "8"
-        set $ws9 "9"
-        set $ws10 "10"
-
-        # switch to workspace
-        bindsym $mod+1 workspace number $ws1
-        bindsym $mod+2 workspace number $ws2
-        bindsym $mod+3 workspace number $ws3
-        bindsym $mod+4 workspace number $ws4
-        bindsym $mod+5 workspace number $ws5
-        bindsym $mod+6 workspace number $ws6
-        bindsym $mod+7 workspace number $ws7
-        bindsym $mod+8 workspace number $ws8
-        bindsym $mod+9 workspace number $ws9
-        bindsym $mod+0 workspace number $ws10
-
-        # move focused container to workspace
-        bindsym $mod+Shift+1 move container to workspace number $ws1
-        bindsym $mod+Shift+2 move container to workspace number $ws2
-        bindsym $mod+Shift+3 move container to workspace number $ws3
-        bindsym $mod+Shift+4 move container to workspace number $ws4
-        bindsym $mod+Shift+5 move container to workspace number $ws5
-        bindsym $mod+Shift+6 move container to workspace number $ws6
-        bindsym $mod+Shift+7 move container to workspace number $ws7
-        bindsym $mod+Shift+8 move container to workspace number $ws8
-        bindsym $mod+Shift+9 move container to workspace number $ws9
-        bindsym $mod+Shift+0 move container to workspace number $ws10
 
 
         # Identify windows with "xprop" or "xwininfo -tree -root"
