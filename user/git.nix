803eecef05bd9dd92020c9761ec2d197689912e5
Maraxi <Maraxi@users.noreply.github.com>
2025-01-07 12:54:30 +0100

    diffs for git

--- user/git.nix
+++ user/git.nix
@@ -11,6 +11,7 @@
       aliases = "config --get-regexp alias";
       pushd = "push -u origin HEAD";
       s = "status";
+      diffs = "diff --staged";
       p = "add -p";
       cm = "commit -m";
       lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cr)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --branches --remotes --tags HEAD";
