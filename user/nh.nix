c3a1af93bd5344331be0a42ea1bfa7800b55a0e6
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-01-14 14:30:11 +0100

    disable nh clean entirely

--- user/nh.nix
+++ user/nh.nix
@@ -1,8 +1,6 @@
 {setup, ...}: {
   programs.nh = {
     enable = true;
-    clean.enable = !setup.isNixOS;
-    clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/${setup.username}/${
       if setup.isNixOS
       then "nixconfig"
