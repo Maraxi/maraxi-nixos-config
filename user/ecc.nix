62aee7094e4d0ab2ca7164bef7254acc56c26cbe
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-01-15 18:00:44 +0100

    use native tls for uv over vpn

--- user/ecc.nix
+++ user/ecc.nix
@@ -3,6 +3,7 @@
     UV_PYTHON = "3.11";
     UV_DEFAULT_INDEX = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
     UV_PYTHON_PREFERENCE = "only-managed";
+    UV_NATIVE_TLS = "true";
     no_proxy = "$no_proxy,api.ocp.dev.eex.energy";
   };
   home.shellAliases = {
