001ea0563a72160c84c213c0b226a317e5e992b2
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-01-20 13:37:46 +0100

    show 10 entries in short git history

--- user/git.nix
+++ user/git.nix
@@ -1,7 +1,7 @@
 {
   home.shellAliases = {
     s = "git status";
-    g = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cr)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' -n5";
+    g = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cr)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' -n10";
   };
   programs.git = {
     enable = true;
