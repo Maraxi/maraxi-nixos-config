7a2933a3a6ef8a63b4e6700fb45b3ae14c0c97b7
Maraxi <Maraxi@users.noreply.github.com>
2025-02-01 13:20:38 +0100

    whitespace

--- user/environment.nix
+++ user/environment.nix
@@ -2,10 +2,12 @@
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
+
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
+
     PYTHON_HISTORY = "$HOME/.local/share/python-history";
     IPYTHON_DIR = "$HOME/.local/share/ipython";
   };
@@ -13,6 +15,7 @@
     "$HOME/bin"
     "$HOME/.local/bin"
   ];
+
   xdg.mimeApps = let
     association = {
       "application/pdf" = ["org.gnome.Evince.desktop" "firefox.desktop"];
