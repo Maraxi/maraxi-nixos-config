f533d156804d3d06c2a7e73adc07eee858b5a8ce
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-02-27 10:59:58 +0100

    update no_proxy on ecc

--- user/ecc.nix
+++ user/ecc.nix
@@ -1,10 +1,10 @@
-{
+{lib, ...}: {
   home.sessionVariables = {
     UV_PYTHON = "3.11";
     UV_DEFAULT_INDEX = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
     UV_PYTHON_PREFERENCE = "only-managed";
     UV_NATIVE_TLS = "true";
-    no_proxy = "$no_proxy,api.ocp.dev.eex.energy";
+    no_proxy = lib.strings.concatStringsSep "," ["$no_proxy" ".dbgcloud.io" ".testing" ".eex.energy" ".deutsche-boerse.de" ".oa.pnrad.net"];
   };
   home.shellAliases = {
     cal = "ncal -b3A3";
