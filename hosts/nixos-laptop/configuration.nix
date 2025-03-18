9387763aba35b85b5d67d6512d12da41c6d8c358
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-03-18 21:55:28 +0100

    enable ntfs on nixos

--- hosts/nixos-laptop/configuration.nix
+++ hosts/nixos-laptop/configuration.nix
@@ -2,10 +2,10 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
-  config,
+  # config,
   lib,
   pkgs,
-  outputs,
+  # outputs,
   ...
 }: {
   imports = [
@@ -29,6 +29,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["ntfs"];
 
   networking.hostName = "stefan-nixos";
   networking.networkmanager.enable = true;
