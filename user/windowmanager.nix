e89feba520d2f8c8cee86307cee206304c2fa1ea
stefan.eysoldt <stefan.eysoldt@ecc.de>
2025-03-31 08:40:46 +0200

    fix outputs on i3

--- user/windowmanager.nix
+++ user/windowmanager.nix
@@ -369,7 +369,7 @@ in
           workspaceOutputAssign =
             map (workspace: {
               workspace = workspace;
-              output = ["DP-1-1.8" "DP-1-1.1" "DP-1-1.1.8" "DP-1-1.3" "DP-1.3" "DP-1.1" "DP-1.8"];
+              output = ["DP-1-1.8" "DP-1-1.1" "DP-1-1.1.8" "DP-1-1.3" "DP-1.3" "DP-1.8"];
             }) ["1" "2" "3"]
             ++ map (workspace: {
               workspace = workspace;
