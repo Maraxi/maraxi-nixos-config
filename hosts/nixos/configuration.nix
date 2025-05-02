0da7137c6dc9c45d13ce9c51d572c6b8cdc10cc1
Maraxi <Maraxi@users.noreply.github.com>
2025-05-02 16:15:08 +0200

    update host config

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,35 +1,46 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  # config,
+  lib,
+  pkgs,
+  config,
+  # outputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../system/greetd.nix
+  ];
 
-  # Bootloader.
+  nix.package = pkgs.nixVersions.stable;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.trusted-users = ["stefan"];
+  # nixpkgs = {
+  #   config = {
+  #     allowBroken = true;
+  #     allowUnfree = true;
+  #   };
+  #   overlays = [
+  #     outputs.overlays.trunk-packages
+  #   ];
+  # };
+
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["ntfs"];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "stefan-nixos";
+  networking.networkmanager.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -43,59 +54,164 @@
   };
 
   # Configure keymap in X11
+  # TODO: use central keyboard setting from flake.nix
   services.xserver.xkb = {
     layout = "de";
-    variant = "";
+    variant = "nodeadkeys";
+    options = "caps:escape_shifted_capslock, compose:sclk";
+  };
+  console.useXkbConfig = true; # use xkb.options in tty.
+
+  # wayland-related
+  security.polkit.enable = true;
+  # hardware.opengl.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [brlaser];
+  };
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Brother_MFC_L2710DW_series";
+      deviceUri = "dnssd://Brother%20MFC-L2710DW%20series._ipp._tcp.local/?uuid=e3248000-80ce-11db-8000-3c2af45641b1";
+      model = "drv:///brlaser.drv/brl2710w.ppd";
+      ppdOptions = {
+        PageSize = "A4";
+        Duplex = "DuplexNoTumble";
+      };
+    }
+  ];
+  # Enable sane for scanner
+  hardware.sane.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
   };
 
-  # Configure console keymap
-  console.keyMap = "de";
+  # Enable sound.
+  # sound.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+  # automount / unmount drives
+  services.devmon.enable = true; # automatic mounting of drives
+  services.gvfs.enable = true; # userspace virtual filesystem
+  services.udisks2.enable = true; # DBus service for applications to query storage devices
+
+  # allow non-free keymapp for voyager
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "keymapp"
+    ];
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+  '';
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.stefan = {
     isNormalUser = true;
     description = "Stefan";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["wheel" "networkmanager" "audio" "video" "scanner" "lp"];
     packages = with pkgs; [];
+    # shell = pkgs.nushell;
+    shell = pkgs.bash;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCrOG3FrTlVDOj5TTe5njEB5JUgJ8jum7GdXGK3wNTMeqogCBd1X0K7vTCw2GikgWeUzjN1BM4WeSIlcjyQWGePIVzUlFUhj98h3N9jsQ6T1W7Gyq6vOF35w3cDQTmLYHgFWeDl0llJzj8PW4iJGhVClaVZSyhEVhc78d8xOLadaDK9IljIHaULgDYcZg4Gx+TwcnNmYRvg9oY5RdPft24IJqFPx+xTRM/suw19uXGtGXFUZ316knTkzQFwt6kjVFg7/S0L8J1G+Vo/uCD8DieM1ZGk2WaFaGsrVOdFp1YKCB/iAJFAnR3GFJ6DsPI9UiwsucY1YkbBrVV7MF18Xug/L4ykya7+nxQ0x5iJencHTHwkT14pwfMTdLvZxrLRgxhS/F0q81z+0qWTrx5AS46gTZLlZgHDFSD0rgBE9aZWzZf1x2SxXWKJoY6kpe5WwroRYcyM2ByzXIjrHIvtKdQPmJswzGEFq+/okWw4v5EHdrPd81WLpc6s4ilas6bWjcCObRRux2WA5iS530Xv/G8snacohl21dZ20+L66fzP44410V48kQPcy2DIiDdy1rzj8iRDbn+DHVidy2CDd4Grhhw9J8of59HxWxd02FE7AAlzvsiDdcvKxvzE2fxeBVJCYatfnL+fn7ptcsqLEYDh6PNZyOunto5J4sJfi5ibYcQ== generated by Keepass2Android"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINH9SWAyoJv7govQkrJRcir2uMV9b8fO+87Y8oN8IJXL ssh key from raspberrypi to fujitsu"
+    ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  environment.variables.EDITOR = "nvim";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim
+    vim
     git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
   ];
 
+  environment.etc = {
+    "papersize".text = "a4";
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.droid-sans-mono
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+    ];
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif" "Source Han Serif"];
+      sansSerif = ["Noto Sans" "Source Han Sans"];
+    };
+  };
+
+  programs.dconf.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
+  programs.gnupg.agent = {
+    enable = true;
+    # pinentryFlavor = "gtk2";
+    enableSSHSupport = true;
+  };
+  # services.dbus.packages = [ pkgs.gcr ];
+  # services.pcscd.enable = true;
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [36969];
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [8000];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  nix.settings.auto-optimise-store = true;
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    # optimise = {
+    #   automatic = true;
+    #   dates = ["weekly"];
+    # };
+  };
 
+  system = {
+    # This option defines the first version of NixOS you have installed on this particular machine,
+    # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+    # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+    # and migrated your data accordingly.
+    # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+    stateVersion = "24.11";
+    # autoUpgrade = {
+    #   enable = true;
+    #   flake = inputs.self.outPath; # TOFix use flake
+    #   dates = "weekly";
+    # };
+  };
 }
