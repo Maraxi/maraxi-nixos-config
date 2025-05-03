e2be613d20b3c67c2b06e5606ed1cb9b75657d78
Maraxi <Maraxi@users.noreply.github.com>
2025-05-03 17:49:34 +0200

    remove unused imports

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,14 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-{
-  # config,
-  lib,
-  pkgs,
-  config,
-  # outputs,
-  ...
-}: {
+{lib, ...}: {
   imports = [./hardware-configuration.nix];
 
   # nixpkgs.overlays = [outputs.overlays.trunk-packages];
