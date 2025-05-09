f791f8c80393b584942dd3b0e83df3d99c3eef69
Maraxi <Maraxi@users.noreply.github.com>
2025-05-09 08:52:28 +0200

    add swap device

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -30,7 +30,7 @@
     options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [];
+  swapDevices = [{device = "/dev/disk/by-uuid/8f8a291d-e4fd-4f77-85a1-9dc1e744a0d0";}];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
