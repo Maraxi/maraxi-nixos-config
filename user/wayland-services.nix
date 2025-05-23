126ca398bf4763bf053ab8372171de26b470f601
Maraxi <Maraxi@users.noreply.github.com>
2025-05-23 22:51:11 +0200

    configure swaylock

--- user/wayland-services.nix
+++ user/wayland-services.nix
@@ -17,5 +17,15 @@
   };
   programs.swaylock = {
     enable = true;
+    settings = {
+      color = "000000";
+      font-size = 24;
+      indicator-idle-visible = true;
+      indicator-radius = 100;
+      indicator-thickness = 15;
+      line-color = "000000";
+      show-failed-attempts = true;
+      ignore-empty-password = true;
+    };
   };
 }
