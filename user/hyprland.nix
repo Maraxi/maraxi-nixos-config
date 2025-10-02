86d9ac1a776d1afe3bc9317706a9fe04ae143452
Maraxi <Maraxi@users.noreply.github.com>
2025-10-02 10:07:23 +0200

    apply shellcheck

--- user/hyprland.nix
+++ user/hyprland.nix
@@ -70,7 +70,7 @@
         }
 
         ${pkgs.socat}/bin/socat -U - \
-          UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock \
+          UNIX-CONNECT:"$XDG_RUNTIME_DIR"/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock \
           | while read -r line; do handle "$line"; done
       ''}";
     };
