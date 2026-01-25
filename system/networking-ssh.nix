b5c3bacb9cb83a9e5260c3f8b2df7d1a96eb7408
Maraxi <Maraxi@users.noreply.github.com>
2026-01-25 10:59:52 +0100

    nftables for firewall

--- system/networking-ssh.nix
+++ system/networking-ssh.nix
@@ -8,11 +8,13 @@
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+    firewall.enable = true;
     # Open ports in the firewall.
-    firewall.allowedTCPPorts = [8000];
+    # firewall.allowedTCPPorts = [8000];
     # firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # firewall.enable = false;
+
+    # nftables instead of iptbles for firewall
+    nftables.enable = true;
   };
 
   # Enable the OpenSSH daemon.
