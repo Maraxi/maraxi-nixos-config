ce1467f8f84907e2ae79d132fa0dd067b9bf188d
Maraxi <Maraxi@users.noreply.github.com>
2026-02-01 20:34:22 +0100

    move hyprrandom to extra folder

--- user/hyprland.nix
+++ user/hyprland.nix
@@ -26,7 +26,7 @@
     paint_mode=rectangle
     early_exit=true
   '';
-  home.file."bin/hyprpaper-random".source = config.lib.meta.mkMutableSymlink dotfiles/hyprpaper-random;
+  home.file."bin/hyprpaper-random".source = config.lib.meta.mkMutableSymlink dotfiles/hypr-extra/hyprpaper-random;
 
   systemd.user.services.hyprland-ipc = {
     Unit = {
