483f096fe52fe9098d6b6c315730da4f27092b66
Maraxi <Maraxi@users.noreply.github.com>
2026-02-01 17:39:32 +0100

    add fman

--- user/packages.nix
+++ user/packages.nix
@@ -128,6 +128,7 @@
   home.file = {
     "bin/create-venv".source = config.lib.meta.mkMutableSymlink dotfiles/bin/create-venv;
     "bin/detektor".source = config.lib.meta.mkMutableSymlink dotfiles/bin/detektor;
+    "bin/fman".source = config.lib.meta.mkMutableSymlink dotfiles/bin/fman;
   };
 
   dconf.settings = {
