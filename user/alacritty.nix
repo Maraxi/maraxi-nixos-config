04cbfbef138ffa5379382bd3fbee90a70ed70605
Maraxi <Maraxi@users.noreply.github.com>
2026-04-11 14:52:43 +0200

    symlink for alacritty

--- user/alacritty.nix
+++ user/alacritty.nix
@@ -1,54 +1,8 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  ...
+}: {
   programs.alacritty.enable = true;
-  programs.alacritty.settings = {
-    # Night Owl
-    colors = lib.mkOverride 500 {
-      bright = {
-        black = "#575656";
-        blue = "#82aaff";
-        cyan = "#7fdbca";
-        green = "#22da6e";
-        magenta = "#c792ea";
-        red = "#ef5350";
-        white = "#ffffff";
-        yellow = "#ffeb95";
-      };
-      cursor = {
-        cursor = "#d6deeb";
-        text = "#011627";
-      };
-      normal = {
-        black = "#011627";
-        blue = "#82aaff";
-        cyan = "#21c7a8";
-        green = "#22da6e";
-        magenta = "#c792ea";
-        red = "#ef5350";
-        white = "#ffffff";
-        yellow = "#c5e478";
-      };
-      primary = {
-        background = "#011627";
-        foreground = "#d6deeb";
-      };
-      selection = {
-        background = "#1b90dd";
-      };
-    };
-    keyboard.bindings = [
-      {
-        action = "SpawnNewInstance";
-        key = "Return";
-        mods = "Control|Shift";
-      }
-    ];
-    window = {
-      dynamic_title = true;
-      opacity = 0.65;
-    };
-    window.padding = {
-      x = 0;
-      y = 0;
-    };
-  };
+  xdg.configFile."alacritty/alacritty.toml".source = config.lib.meta.mkMutableSymlink dotfiles/alacritty.toml;
 }
