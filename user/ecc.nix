14c52e9305ad02b5a263affcd34bd19928ef43c3
Maraxi@pc9d217 <Maraxi@users.noreply.github.com>
2026-04-21 17:23:47 +0200

    update mimeapps.list

--- user/ecc.nix
+++ user/ecc.nix
@@ -14,6 +14,7 @@
   xdg.mimeApps = let
     association = {
       "application/x-remmina" = "xfreerdp-wrapper.desktop";
+      "x-scheme-handler/jetbrains" = "jetbrainsd.desktop";
     };
   in {
     defaultApplications = association;
