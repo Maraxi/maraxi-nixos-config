dad66d4232dd1f41ce4f7d86c30970dab095be11
Maraxi <Maraxi@users.noreply.github.com>
2026-06-02 13:26:25 +0200

    multiline greetd again

--- system/greetd.nix
+++ system/greetd.nix
@@ -1,8 +1,20 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --user-menu --remember --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red --cmd Hyprland";
+      default_session.command = lib.strings.join " " [
+        "${pkgs.tuigreet}/bin/tuigreet"
+        "--time"
+        "--asterisks"
+        "--user-menu"
+        "--remember"
+        "--theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"
+        "--cmd Hyprland"
+      ];
     };
     useTextGreeter = true;
   };
