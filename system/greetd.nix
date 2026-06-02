0ed9637daf15df57a7540de4c1a8c743c7cd3337
Maraxi <Maraxi@users.noreply.github.com>
2026-06-02 09:39:22 +0200

    greetd no interrupting boot messages

--- system/greetd.nix
+++ system/greetd.nix
@@ -4,6 +4,7 @@
     settings = {
       default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --user-menu --remember --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red --cmd Hyprland";
     };
+    useTextGreeter = true;
   };
 
   environment.etc."greetd/environments".text = ''
