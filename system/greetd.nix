162e0775f7453fe877e06d158f26e02575b99ce1
Maraxi <Maraxi@users.noreply.github.com>
2026-06-02 14:26:58 +0200

    use replaceString in greetd

--- system/greetd.nix
+++ system/greetd.nix
@@ -6,15 +6,14 @@
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = lib.strings.join " " [
-        "${pkgs.tuigreet}/bin/tuigreet"
-        "--time"
-        "--asterisks"
-        "--user-menu"
-        "--remember"
-        "--theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"
-        "--cmd Hyprland"
-      ];
+      default_session.command = lib.replaceString "\n" " " ''
+        ${pkgs.tuigreet}/bin/tuigreet
+        --time
+        --asterisks
+        --user-menu
+        --remember
+        --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red
+        --cmd Hyprland'';
     };
     useTextGreeter = true;
   };
