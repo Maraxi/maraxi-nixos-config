6a90fcbef665adea795e93e936cd2c34efa2acbf
Maraxi@pc9d217 <Maraxi@users.noreply.github.com>
2026-06-16 21:38:31 +0200

    config file for bat

--- user/shells.nix
+++ user/shells.nix
@@ -145,6 +145,8 @@
   programs.lesspipe.enable = true;
   xdg.configFile."lesskey".source = config.lib.meta.mkMutableSymlink dotfiles/lesskey;
 
+  xdg.configFile."bat".source = config.lib.meta.mkMutableSymlink dotfiles/bat;
+
   xdg.dataFile."ipython/profile_default/ipython_config.py".text = ''
     c = get_config()
     c.TerminalInteractiveShell.confirm_exit = False
