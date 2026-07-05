6d1e98865c71f90a227f4f78d309e6405dff0a86
Maraxi <Maraxi@users.noreply.github.com>
2026-07-05 23:27:50 +0200

    changes to nvim config

--- user/dotfiles/nvim/init.lua
+++ user/dotfiles/nvim/init.lua
@@ -1,13 +1,16 @@
 -- #######################
 -- ##      OPTIONS      ##
 -- #######################
--- vim.cmd([[set mouse=]])  -- need to fix jump, usually ^] / :tag
+
+-- TODO: test: vim.loader.enable()
+-- vim.cmd([[set mouse=]])  -- TODO: need to fix jump, usually ^] / :tag
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.opt.undofile = true
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.title = true
 
 vim.opt.tabstop = 8
 vim.opt.shiftwidth = 8
@@ -27,9 +30,33 @@ vim.opt.smartcase = true
 vim.opt.scrolloff = 20
 vim.opt.sidescrolloff = 10
 
+-- #######################
+-- ##      PLUGINS      ##
+-- #######################
+
+vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
+  local name, kind = ev.data.spec.name, ev.data.kind
+  if name == 'nvim-treesitter' and kind == 'update' then
+    if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
+    vim.cmd('TSUpdate')
+  end
+end })
+
+vim.pack.add {
+  { src = 'https://github.com/folke/tokyonight.nvim' },
+  { src = 'https://github.com/nvim-mini/mini.nvim' },
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+  -- { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+}
+
+vim.cmd.colorscheme 'tokyonight'
+
+-- TODO: https://tduyng.com/blog/neovim-git-tools/
+
 -- ########################
 -- ##      KEYBINDS      ##
 -- ########################
+
 -- Run lines in lua
 vim.keymap.set('n', '<leader><leader>x', '<cmd>source $MYVIMRC<CR>')
 vim.keymap.set('n', '<leader>x', ':.lua<CR>')
@@ -38,20 +65,12 @@ vim.keymap.set('v', '<leader>x', ':lua<CR>')
 -- #######################
 -- ##      SCRIPTS      ##
 -- #######################
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
+  -- TODO: do I need the group?
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
-
-print 'init.lua was loaded'
-
--- #######################
--- ##      PLUGINS      ##
--- #######################
-vim.pack.add { 'https://github.com/nvim-mini/mini.nvim' }
