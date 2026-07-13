58a8d12982db803d59ce0a3b675cbd47f83f74af
Maraxi@pc9d217 <Maraxi@users.noreply.github.com>
2026-07-13 11:15:50 +0200

    add dotfiles for p1

--- user/ecc.nix
+++ user/ecc.nix
@@ -1,4 +1,4 @@
-{
+{config, ...}: {
   home.sessionVariables = {
     UV_DEFAULT_INDEX = "https://artifactory.dbgcloud.io/artifactory/api/pypi/cio-ecc-itsdesign-pypi-dev/simple";
     UV_PYTHON = "3.13";
@@ -6,6 +6,9 @@
     UV_SYSTEM_CERTS = "true";
     TNS_ADMIN = "$HOME/.config/tns_ora";
   };
+
+  home.sessionPath = ["$HOME/p1-bin"];
+  home.file."p1-bin".source = config.lib.meta.mkMutableSymlink ../hosts/p1;
 
   xdg.mimeApps = let
     association = {
