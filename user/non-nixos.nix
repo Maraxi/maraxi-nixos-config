e22b7d615d8ff6c5ab60f2830f1cb265287601de
Maraxi@pc9d217 <Maraxi@users.noreply.github.com>
2026-07-13 12:55:33 +0200

    update nvidia driver version

--- user/non-nixos.nix
+++ user/non-nixos.nix
@@ -1,10 +1,13 @@
 {
+  # https://github.com/nix-community/home-manager/blob/master/docs/manual/usage/gpu-non-nixos.md
   targets.genericLinux = {
     enable = true;
     gpu.nvidia = {
       enable = false;
-      version = "595.58.03";
-      sha256 = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
+      # Check version in
+      # nvidia-smi -q | grep "Driver Version"
+      version = "595.71.05";
+      sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
     };
   };
 }
