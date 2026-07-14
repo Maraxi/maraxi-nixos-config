1640b0403a283e035e3c07ac2ec324c3d0a430d0
Maraxi@pc9d217 <Maraxi@users.noreply.github.com>
2026-07-14 12:47:34 +0200

    install hicolor theme for icons

--- user/windowmanager.nix
+++ user/windowmanager.nix
@@ -476,6 +476,7 @@ in
       enable = true;
       configFile = config.lib.meta.mkMutableSymlink dotfiles/dunstrc;
     };
+    home.packages = [pkgs.hicolor-icon-theme];
     xdg.configFile."dunst/dunstrc".enable = false;
     xdg.configFile."dunst/readme".text = "Managed by home-manager. See the config used by service instead:\nsystemctl --user status dunst.service";
   }
