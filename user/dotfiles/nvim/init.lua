902748430633b416100cca569de9d87b17b3ab84
Maraxi <Maraxi@users.noreply.github.com>
2026-07-15 01:17:59 +0200

    require telescope

--- user/dotfiles/nvim/init.lua
+++ user/dotfiles/nvim/init.lua
@@ -186,3 +186,16 @@ statusline.setup {}
 -- TODO: check if needed:
 -- https://github.com/NMAC427/guess-indent.nvim
 -- require('guess-indent').setup {}
+
+-- ############################################
+-- ##      Search / Navigation Plug ins      ##
+-- ############################################
+
+vim.pack.add {
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+}
+
+-- TODO: https://tduyng.com/blog/neovim-git-tools/
