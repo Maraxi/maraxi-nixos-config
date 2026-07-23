1bc59f48315f8c6cc66d66cbc98f42f79a6d5cf4
Maraxi@pc9d217 <Maraxi@users.noreply.github.com>
2026-07-23 10:25:55 +0200

    i3status file

--- user/windowmanager.nix
+++ user/windowmanager.nix
@@ -470,6 +470,7 @@ in
       '';
     };
 
+    xdg.configFile."i3status/config".source = dotfiles/i3/i3status;
     xdg.configFile."i3/edge.json".source = dotfiles/i3/edge.json;
 
     services.dunst = {
