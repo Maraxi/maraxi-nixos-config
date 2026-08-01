4768eb0e62ba969bd36b0418764f49b0e841eda9
Maraxi <Maraxi@users.noreply.github.com>
2026-08-01 15:33:58 +0200

    force enable primary paste

--- user/packages.nix
+++ user/packages.nix
@@ -160,6 +160,7 @@
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
+      gtk-enable-primary-paste = true;
       color-scheme = "prefer-dark";
     };
     "org/nemo/preferences" = {
