650096c7eb5b2a78d30183d289675a271e0af62e
Maraxi <Maraxi@users.noreply.github.com>
2026-08-14 17:13:56 +0200

    dunst via home-manager

--- user/notifications.nix
+++ user/notifications.nix
@@ -1,12 +1,17 @@
 { config, pkgs, ... }: {
   services.dunst = {
     enable = true;
-    configFile = config.lib.meta.mkMutableSymlink dotfiles/dunstrc;
+    # configFile = config.lib.meta.mkMutableSymlink dotfiles/dunstrc;
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+      size = "32x32";
+    };
   };
-  home.packages = [
-    pkgs.papirus-icon-theme
-  ];
-  xdg.configFile."dunst/dunstrc".enable = false;
-  xdg.configFile."dunst/readme".text =
-    "Managed by home-manager. See the config used by service instead:\nsystemctl --user status dunst.service";
+  # home.packages = [
+  # pkgs.papirus-icon-theme
+  # ];
+  # xdg.configFile."dunst/dunstrc".enable = false;
+  # xdg.configFile."dunst/readme".text =
+  # "Managed by home-manager. See the config used by service instead:\nsystemctl --user status dunst.service";
 }
