6ad7e99b7c1f6856a7cec63b52489c119d26ecb9
Maraxi <Maraxi@users.noreply.github.com>
2026-08-14 02:07:31 +0200

    update voyager rule

--- system/voyager.nix
+++ system/voyager.nix
@@ -2,7 +2,7 @@
   # udev rules for voyager keyboard
   services.udev.extraRules = ''
     KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0660", GROUP="plugdev", SYMLINK+="ignition_dfu"
   '';
   environment.systemPackages = [ pkgs.keymapp ];
 }
