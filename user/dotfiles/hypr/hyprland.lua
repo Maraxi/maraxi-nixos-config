a5f3c94d1ec46430e688f30b9fa9edf9f8ac18bb
Maraxi <Maraxi@users.noreply.github.com>
2026-09-01 15:39:01 +0200

    mod+space to focus float

--- user/dotfiles/hypr/hyprland.lua
+++ user/dotfiles/hypr/hyprland.lua
@@ -239,6 +239,16 @@ hl.bind(mehMod .. 'Q', hl.dsp.window.close())
 hl.bind(mehMod .. 'K', hl.dsp.window.kill())
 
 hl.bind(mehMod .. 'L', hl.dsp.window.float { action = 'toggle' })
+hl.bind(
+  mainMod .. 'space',
+  function()
+    hl.dispatch(hl.dsp.window.cycle_next {
+      floating = not hl.get_active_window().floating,
+    })
+  end,
+  { description = 'Switch focus between tiled and floating windows' }
+)
+
 -- hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 -- hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
 
