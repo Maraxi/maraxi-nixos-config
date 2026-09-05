f9cd8b7813f60ba5e0fbb3047695a8b1181a9b95
Maraxi <Maraxi@users.noreply.github.com>
2026-09-05 16:04:58 +0200

    move workspace rule

--- user/dotfiles/hypr/hyprland.lua
+++ user/dotfiles/hypr/hyprland.lua
@@ -154,24 +154,6 @@ hl.animation { leaf = 'workspacesOut', enabled = true, speed = 1.94, bezier = 'a
 hl.animation { leaf = 'zoomFactor', enabled = true, speed = 7, bezier = 'quick' }
 --]]
 
--- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
--- "Smart gaps" / "No gaps when only"
--- uncomment all if you wish to use that.
--- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
-hl.workspace_rule { workspace = 'f[1]', gaps_out = 0, gaps_in = 0 }
--- hl.window_rule({
---     name  = "no-gaps-wtv1",
---     match = { float = false, workspace = "w[tv1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
--- hl.window_rule({
---     name  = "no-gaps-f1",
---     match = { float = false, workspace = "f[1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
-
 ----------------
 ----  MISC  ----
 ----------------
@@ -419,6 +401,10 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-- "Smart gaps" / "No gaps when only"
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule { workspace = 'f[1]', gaps_out = 0, gaps_in = 0 } --maximized window
 
 -- Workspaces on fixed monitors with default apps
 hl.workspace_rule { workspace = '2', monitor = monitor_right, on_created_empty = 'firefox' }
