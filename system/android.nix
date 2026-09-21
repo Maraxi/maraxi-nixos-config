f173bc832a0660f087a2491e359526557ba3b175
Maraxi <Maraxi@users.noreply.github.com>
2026-09-22 00:17:17 +0200

    only install adb on demand

--- system/android.nix
+++ system/android.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
-  environment.systemPackages = [ pkgs.android-tools ];
+  # Only install android-tools on demand
+  # environment.systemPackages = [ pkgs.android-tools ];
   users.users.stefan.extraGroups = [ "adbusers" ];
 }
