901fe8e5886af83e389fa06db89415d0047b4c6a
Maraxi <Maraxi@users.noreply.github.com>
2026-09-25 22:38:59 +0200

    schedule clipboard instead of direct load

--- user/dotfiles/nvim/init.lua
+++ user/dotfiles/nvim/init.lua
@@ -18,9 +18,9 @@ do
   -- Decrease update time, debounce this long until writing to swap
   vim.o.updatetime = 250
 
-  vim.o.clipboard = 'unnamedplus'
-  -- TODO: check if this makes a difference
-  -- vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+  -- Sync clipboard between OS and Neovim.
+  -- Schedule the setting after `UiEnter` because it can increase startup-time.
+  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
   --    #### window decorations, etc. ####
   -- Don't show the mode, since it's already in the status line
